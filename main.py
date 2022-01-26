75bcfe7694b0c14e979a0f17ab5584b763c43d39
Sebastian Klang <sebklang@gmail.com>
2022-01-26 23:09:48 +0100

    specified margins

--- main.py
+++ main.py
@@ -66,5 +66,5 @@ for tl in ax2.get_yticklabels():
 ax1.legend(loc='upper left')
 ax2.legend(loc='upper right')
 plt.title(label='Edits over time to Wikipedia article $\it{Russo-Ukrainian}$ $\it{War}$')
-fig.savefig('figure.png')
+fig.savefig('figure.png', bbox_inches='tight', pad_inches=0.45)
 plt.show()
